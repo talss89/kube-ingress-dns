54a89ab54f4b8bcc8a2588d561c7a7960bcacdff
Tom Lawton <tom.lawton@cndu.it>
2024-01-10 13:41:23 +0000

    feat: wildcard support for ingress host rule

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
 use std::env;
 use simple_logger::SimpleLogger;
+use wildmatch::WildMatch;
 
 use k8s_openapi::api::networking::v1::Ingress;
 
@@ -702,7 +703,7 @@ async fn search_ingresses(qname: &str) -> Option<Ipv4Addr> {
                 if let Some(rules) = &spec.rules {
                     for r in rules.iter() {
                         if let Some(host) = &r.host {
-                            if *host == qname.to_string() {
+                            if WildMatch::new(&*host).matches(qname) {
                                 log::info!("Found {} matching {}", ingress.metadata.name?, qname);
                                 return Some(Ipv4Addr::from_str(
                                     ingress.status.as_ref()?.load_balancer.as_ref()?.ingress.as_ref()?[0].ip.as_ref()?
